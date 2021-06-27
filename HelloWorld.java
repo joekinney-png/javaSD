99e0253dacba3cbe5dc9b92d37c4eeee78640f2b
Joe Kinney <joe@enterpagoda.com>
2021-06-27 16:36:14 -0400

    hello world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// every line of code that runs needs to be inside a class
+// public means any other class can access this class
+// when declaring a public class, it must be inside a file with the same name
+public class HelloWorld {
+    // entry point of program
+    // anyone can access it, can run this method without creating an instance of main and the method doesn't return a value
+    // main is the name of the method
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
